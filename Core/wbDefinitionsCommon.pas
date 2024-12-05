f8cf0ba3fc6e09759c54b166c4f62663892915f9
robertgk2017 <robertgk2017@gmail.com>
2024-12-04 23:35:15 -0500

    Update wbBlendModeEnum and wbBlendOpEnum

--- Core/wbDefinitionsCommon.pas
+++ Core/wbDefinitionsCommon.pas
@@ -4912,27 +4912,27 @@ begin
     ]);
 
   wbBlendModeEnum :=
-    wbEnum([
-      {1}  'Zero',
-      {2}  'One',
-      {3}  'Source Color',
-      {4}  'Source Inverse Color',
-      {5}  'Source Alpha',
-      {6}  'Source Inverted Alpha',
-      {7}  'Dest Alpha',
-      {8}  'Dest Inverted Alpha',
-      {9}  'Dest Color',
-      {10} 'Dest Inverse Color',
-      {11} 'Source Alpha SAT'
+    wbEnum([], [
+      1,  'Zero',
+      2,  'One',
+      3,  'Source Color',
+      4,  'Source Inverse Color',
+      5,  'Source Alpha',
+      6,  'Source Inverted Alpha',
+      7,  'Dest Alpha',
+      8,  'Dest Inverted Alpha',
+      9,  'Dest Color',
+      10, 'Dest Inverse Color',
+      11, 'Source Alpha SAT'
     ]);
 
   wbBlendOpEnum :=
-    wbEnum([
-      {1} 'Add',
-      {2} 'Subtract',
-      {3} 'Reverse Subtract',
-      {4} 'Minimum',
-      {5} 'Maximum'
+    wbEnum([], [
+      1, 'Add',
+      2, 'Subtract',
+      3, 'Reverse Subtract',
+      4, 'Minimum',
+      5, 'Maximum'
     ]);
 
   wbBodyLocationEnum :=
